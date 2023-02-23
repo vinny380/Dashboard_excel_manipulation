af64aa66c458e28bb092aa80b757d0fe84326908
Your Name <vinny.purgato@gmail.com>
2023-02-22 21:21:52 -0500

    everything working

--- excel.py
+++ excel.py
@@ -8,49 +8,87 @@ st.image('https://i.ibb.co/L0hHCFZ/Screenshot-2023-02-22-at-12-57-43-PM.png')
 st.markdown("Automated Excel Editor")
 excel_file = st.file_uploader('Upload your excel file')
 
-def pre_process_excel():
-    wb = openpyxl.load_workbook(BytesIO(excel_file.read()))
-    sheet = wb.active #specify the sheet name to select other than the active sheet
-    print("Maximum rows before removing : ",sheet.max_row)
-    sheet.insert_cols(1, 2)
-    sheet.insert_cols(idx=4)
-    sheet.delete_rows(0,17)
-    sheet['A1'] = "Subject Number"
-    sheet['B1'] = 'list'
-    sheet['D1'] = 'null'
-    for row in sheet.iter_rows():
-        values = [cell.value for cell in row]
-        values[4], values[10] = values[10], values[4]
-        for i, cell in enumerate(row):
-            cell.value = values[i]
-    # wb.save()
-    return sheet
+def same_choice(p_column, j_column):
+    result = []
+    for p, j in zip(p_column, j_column):
+        if p.value == j.value:
+            result.append(1)
+        elif p.value == "don't know":
+            result.append(0.5)
+        else:
+            result.append(0)
+    return result
 
+def choice(m_column, l_column, k_column):
+    result = []
+    for m, l, k in zip(m_column, l_column, k_column):
+        if m.value == 'j':
+            result.append(l.value)
+        elif m.value == 'f':
+            result.append(k.value)
+        elif m.value == 'd':
+            result.append('don\'t know')
+        else:
+            result.append(None)
+    return result
+
+def get_belief_type(column):
+    results = []
+    for cell in column:
+        string = str(cell.value)
+        results.append(string[-1])
+    return results
 
 def edit_excel():
+
+  wb_old = openpyxl.load_workbook(BytesIO(excel_file.read()))
+  ws_old = wb_old.active #specify the ws_old name to select other than the active ws_old
+  number = int(ws_old['A12'].value.replace('Subject Number: ', ''))
+  print("Maximum rows before removing : ",ws_old.max_row)
+  ws_old.insert_cols(1, 2)
+  ws_old.insert_cols(idx=4)
+  ws_old.delete_rows(0,17)
+
+  ws_old['A1'] = "Subject Number"
+  ws_old['B1'] = 'list'
+  ws_old['D1'] = 'null'
+  for row in ws_old.iter_rows():
+      values = [cell.value for cell in row]
+      values[4], values[10] = values[10], values[4]
+      for i, cell in enumerate(row):
+          cell.value = values[i]
+          
   path1 = 'edited_.xlsx'
+#   number = subject_number()
   wb = openpyxl.Workbook()
-  ws_old = pre_process_excel()
   column_values = ['Subject Number', 'list', 'trial', 'null', 'condition', 'time',
                    'Relationship',
                    'ControlQ1 Copy 2', 'ControlQ1 Copy - 2 - 2',
-                   'FirstMoozleProp Copy 13',
-                  'SecondMoozleProp Copy 13', 'SecondMoozleProp2 Copy 13','ChoiceResponse Copy 2',
-                  'ControlQ2 Copy 2', 'ControlQ2 Copy-2 - 2', 'Choice','SameChoice ', 'BeliefType', 'AgeGroup']
+                   'FirstMoozleProp Copy 13', 'SecondMoozleProp Copy 13',
+                   'SecondMoozleProp2 Copy 13','ChoiceResponse Copy 2',
+                  'ControlQ2 Copy 2', 'ControlQ2 Copy-2 - 2', 'Choice','SameChoice',
+                   'BeliefType', 'AgeGroup']
   ws = wb.active
-
 
   for index, value in enumerate(column_values, start=1):
       cell = ws.cell(row=1, column=index)
       cell.value = value
       wb.save(path1)
 
+  # ws_old.delete_rows(0,17)
   i = 2 # sample numbers start at 2
   j = 11 # Every sample has 9 trials, hence the hence sample is i + 9 = 11
   while i <= 25:
     for cell in ws_old['C']:
         if cell.value == i:
             if i == 2:
+
+              #Choice
+              ws.cell(row=i, column=16).value = choice(ws['M'], ws['L'], ws['K'])[i-2]
+
+              # Subject Number
+              ws.cell(row=i, column=1).value = number
+
               # Trial
               string = ws_old.cell(row=i, column=3).value
               ws.cell(row=i, column=3).value = string
@@ -103,6 +141,9 @@ def edit_excel():
 
               i += 1
             else:
+              # Subject Number
+              ws.cell(row=i, column=1).value = number              
+
               # Trial
               string = ws_old.cell(row=j, column=3).value
               ws.cell(row=i, column=3).value = string
@@ -156,6 +197,13 @@ def edit_excel():
 
               i += 1
               j += 9  
+
+  for i in range(24):
+      # print(choice(ws['M'], ws['L'], ws['K'])[i+1])
+      ws.cell(row=i+2, column=16).value = choice(ws['M'], ws['L'], ws['K'])[i+1]
+      ws.cell(row=i+2, column=17).value = same_choice(ws['P'], ws['J'])[i+1]
+      ws.cell(row=i+2, column=18).value = get_belief_type(ws['E'])[i+1]
+
   path1 = 'edited_.xlsx'
   wb.save(path1)
   st.download_button(
@@ -164,9 +212,6 @@ def edit_excel():
         file_name="workbook.xlsx",
         mime="xlsx"
     )
-
-
-
   
       
 edit = st.button('Click me')
